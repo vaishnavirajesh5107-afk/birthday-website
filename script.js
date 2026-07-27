7617e7c1c3a81f875bb071c85c5e3a6188b1ffc4
vaishnavirajesh5107-afk <vaishnavirajesh5107@gmail.com>
2026-07-27 19:17:17 +0530

    Fixed image path

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ function nextPhoto() {
     if (currentPhoto <= 10) {
 
         document.getElementById("galleryImage").src =
-        "photo/Photo " + currentPhoto + ".jpg";
+        "images/Photo " + currentPhoto + ".jpg";
 
         document.getElementById("galleryCaption").innerText = 
         captions[currentPhoto - 1];
